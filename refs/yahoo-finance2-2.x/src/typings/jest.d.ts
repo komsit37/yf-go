867a2bbd71fa6d5930b40d49d299c7726e75e27a
pkomsit <pkomsit@folio-sec.com>
2025-10-10 14:04:02 +0900

    remove yahoo finance files

--- refs/yahoo-finance2-2.x/src/typings/jest.d.ts
+++
@@ -1,9 +0,0 @@
-export {};
-declare global {
-  namespace jest {
-    interface Matchers<R> {
-      // jest-tobetype
-      toBeType(type: string): R;
-    }
-  }
-}
--- refs/yahoo-finance2-2.x/tests/summary-reporter.js
+++
@@ -1,3 +0,0 @@
-//summary-reporter.js
-const { SummaryReporter } = require("@jest/reporters");
-module.exports = SummaryReporter;
